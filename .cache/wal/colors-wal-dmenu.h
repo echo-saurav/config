eec10bb5b30093fa83210d57123c488c29267331
echo-saurav <echo.saurav@gmail.com>
2021-11-13 21:04:23 +0600

    remove wal cache from git

--- .cache/wal/colors-wal-dmenu.h
+++
@@ -1,6 +0,0 @@
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#c99d8a", "#001C2D" },
-	[SchemeSel] = { "#c99d8a", "#662A40" },
-	[SchemeOut] = { "#c99d8a", "#96442C" },
-};
--- .cache/wal/colors-wal-tabbed.h
+++
@@ -1,6 +0,0 @@
-static const char* selbgcolor   = "#001C2D";
-static const char* selfgcolor   = "#c99d8a";
-static const char* normbgcolor  = "#863C32";
-static const char* normfgcolor  = "#c99d8a";
-static const char* urgbgcolor   = "#662A40";
-static const char* urgfgcolor   = "#c99d8a";
